51957965058d36cf5b81ab2c5b3ff442431097a8
JonNesvold <jnesvold@neomedsys.io>
2025-09-19 17:21:34 +0200

    ya

--- pkgs/librewolf-with-policies.nix
+++ pkgs/librewolf-with-policies.nix
@@ -1,4 +1,3 @@
-# pkgs/librewolf-with-policies.nix
 { pkgs }:
 
 pkgs.librewolf.overrideAttrs (oldAttrs: {
@@ -8,5 +7,15 @@ pkgs.librewolf.overrideAttrs (oldAttrs: {
     mkdir -p $out/lib/librewolf/distribution
     # Copy the Emperor's decree directly into the package
     cp ${../configs/librewolf/policies.json} $out/lib/librewolf/distribution/policies.json
+
+    # Copy the entire chrome directory for styling
+    cp -r ${../configs/librewolf/chrome} $out/lib/librewolf/distribution/
+
+    # Create directories for autoconfig
+    mkdir -p $out/lib/librewolf/defaults/pref
+
+    # Copy the autoconfig files
+    cp ${../configs/librewolf/autoconfig.js} $out/lib/librewolf/defaults/pref/autoconfig.js
+    cp ${../configs/librewolf/librewolf.cfg} $out/lib/librewolf/librewolf.cfg
   '';
 })
