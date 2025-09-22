3a60c1c44d23b2b440aeed717276e84dc80d2873
JonNesvold <jnesvold@neomedsys.io>
2025-09-22 22:44:11 +0200

    reatrd claude

--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -2,49 +2,19 @@
 {
   programs.firefox = {
     enable = lib.mkForce true;
+    package = pkgs.librewolf;
     
-    # Override LibreWolf package to include autoconfig files
-    package = pkgs.librewolf.overrideAttrs (oldAttrs: {
-      postInstall = (oldAttrs.postInstall or "") + ''
-        # Set up autoconfig.js
-        mkdir -p $out/lib/librewolf/defaults/pref
-        cat > $out/lib/librewolf/defaults/pref/autoconfig.js << 'EOF'
-pref("general.config.filename", "librewolf.cfg");
-pref("general.config.obscure_value", 0);
-pref("general.config.sandbox_value", 0);
-EOF
-
-        # Set up librewolf.cfg
-        cat > $out/lib/librewolf/librewolf.cfg << 'EOF'
-// Any comment will do, the first line is ignored.
-// Enable userChrome.css customizations
-pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-EOF
-      '';
-    });
-    
-    # Merged policies from both configs/librewolf/policies.json and pkgs/librewolf-with-policies.nix
+    # Just the policies - no package override
     policies = {
-      # Hardware and performance
-      HardwareAcceleration = {
-        Enabled = true;
-      };
-      
-      # Privacy settings
+      HardwareAcceleration.Enabled = true;
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
       DisablePocket = true;
-      
-      # UI preferences
       DefaultTheme = "firefox-compact-dark@mozilla.org";
       
-      # Preferences from both sources merged
       Preferences = {
-        # From configs/librewolf/policies.json
         "browser.startup.homepage" = "about:blank";
         "gfx.webrender.all" = true;
-        
-        # From pkgs/librewolf-with-policies.nix
         "toolkit.legacyUserProfileCustomizations.stylesheets" = {
           Value = true;
           Status = "locked";
@@ -61,7 +31,7 @@ EOF
     };
   };
 
-  # Set up userChrome.css for any existing LibreWolf profiles
+  # Simple userChrome.css setup - no autoconfig complexity
   system.userActivationScripts.librewolf-chrome = ''
     mkdir -p ~/.librewolf
     for profile_dir in ~/.librewolf/*; do
